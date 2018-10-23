4d7b8d419c11112063688f439549b63be5dd66f1
Cody Taft <cody.taft@gmail.com>
2018-10-23 17:37:28 -0600

    Add donate button to app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  handleClick = e => {
+    console.log('hi');
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">Welcome to Dress the Child</header>
+        <button onClick={this.handleClick} buttonType="donate-btn">
+          Donate
+        </button>
       </div>
     );
   }
