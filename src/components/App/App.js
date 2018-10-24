5f43284551c4af29a3f0912c7a861e39b6f726cc
Cody Taft <cody.taft@gmail.com>
2018-10-24 17:37:32 -0600

    Add router to header image

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, withRouter } from 'react-router-dom';
+import { BrowserRouter as Router, withRouter, NavLink } from 'react-router-dom';
 import Routes from '../../components/Routes/Routes';
 import MainContainer from '../../components/MainContainer/MainContainer';
 import './App.css';
 
 class App extends Component {
+  handleClick = () => {};
   render() {
     return (
       <Router>
         <div className="app">
-          <header className="app-header" />
+          <NavLink exact to="/" className="app-header" />
           <main className="main-container">
             <MainContainer />
             <Routes />
