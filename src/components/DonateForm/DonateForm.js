e797b461a803d0fca568ee4e37c3ce704d45a3b6
Cody Taft <cody.taft@gmail.com>
2018-10-24 13:39:46 -0600

    Create input fields in submit form

--- src/components/DonateForm/DonateForm.js
+++ src/components/DonateForm/DonateForm.js
@@ -6,29 +6,87 @@ import './DonateForm.css';
 class DonateForm extends Component {
   constructor(props) {
     super(props);
-    this.state = { complete: false };
+    this.state = {
+      complete: false,
+      name: '',
+      phone: '',
+      email: '',
+      city: '',
+      state: ''
+    };
   }
 
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
   submit = async e => {
+    const { name, phone, email, city, state } = this.state;
     e.preventDefault();
     let { token } = await this.props.stripe.createToken({ name: 'Name' });
+    console.log(token);
     let response = await fetch('/charge', {
       method: 'POST',
       headers: { 'Content-Type': 'text/plain' },
-      body: token.id
+      body: {
+        tokenId: token.id,
+        name,
+        phone,
+        email,
+        city,
+        state
+      }
     });
 
     if (response.ok) console.log('Purchase Complete!');
   };
 
   render() {
-    if (this.state.complete) return <h1>Purchase Complete</h1>;
+    const { complete, name, phone, email, city, state } = this.state;
+    if (complete) return <h1>Purchase Complete</h1>;
     return (
-      <div className="donate-form">
+      <form onSubmit={this.submit} className="donate-form">
         <p>Would you like to complete donation?</p>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          name="name"
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Phone"
+          value={phone}
+          name="phone"
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          name="email"
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          placeholder="City"
+          value={city}
+          name="city"
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          placeholder="State"
+          value={state}
+          name="state"
+          onChange={this.handleChange}
+        />
+        <p>*Information for non-profit records only*</p>
         <CardElement />
-        <button onClick={this.submit}>Send</button>
-      </div>
+        <button>Send</button>
+      </form>
     );
   }
 }
