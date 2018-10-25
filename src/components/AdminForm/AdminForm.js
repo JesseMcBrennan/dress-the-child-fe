b9de0d9c9e7390f0c31699bc1b6cff1a56ed9c41
Cody Taft <cody.taft@gmail.com>
2018-10-25 17:54:07 -0600

    Add adminForm

--- src/components/AdminForm/AdminForm.js
+++ src/components/AdminForm/AdminForm.js
@@ -3,21 +3,39 @@ import React, { Component } from 'react';
 import './AdminForm.css';
 
 class AdminForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      login: '',
+      password: ''
+    };
+  }
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
   render() {
     return (
       <form className="login-form">
         <input
           type="text"
+          name="login"
+          value={this.state.login}
           placeholder="Login"
           className="login-input form-input"
+          onChange={this.handleChange}
         />
         <input
           type="text"
+          name="password"
+          value={this.state.password}
           placeholder="Password"
           className="password-input form-input"
           form-input
+          onChange={this.handleChange}
         />
-        <button>Login</button>
+        <input type="submit" value="Login" />
       </form>
     );
   }
