3edb4a7716fa73418319ba15c2aa6332b05f24c2
Jesse McFadden <jessemcbrennan@gmail.com>
2018-10-25 17:56:40 -0600

    Add links to NavMain

--- src/components/NavMain/NavMain.js
+++ src/components/NavMain/NavMain.js
@@ -5,6 +5,13 @@ const NavMain = () => {
   return(
     <div className="navMain">
       <div className="navLogo"></div>
+        <ul className="NavMain-buttons">
+          <li><a href="">HOME</a></li>
+          <li><a href="">ABOUT</a></li>
+          <li><a href="">DONATE</a></li>
+          <li><a href="">OUR MISSON</a></li>
+          <li><a href="">PARTNERS</a></li>
+        </ul>
     </div>
   )
 }
