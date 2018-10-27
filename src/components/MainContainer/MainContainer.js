9437fec1283cd0cf00c5db1d300c88c988010f5a
Cody Taft <cody.taft@gmail.com>
2018-10-27 16:54:44 -0600

    Style main card container

--- src/components/MainContainer/MainContainer.js
+++ src/components/MainContainer/MainContainer.js
@@ -10,16 +10,24 @@ export class MainContainer extends Component {
     return (
       <section className="main-container">
         <div className="display-card">
-          <NavLink
-            exact
-            to="/donate-page"
-            className={
-              location.pathname !== '/' ? 'donate-btn-hidden' : 'donate-btn'
-            }
-          >
-            Donate
-          </NavLink>
+          <h1 className="main-title">DRESS THE CHILD</h1>
+          <hr className="horizontal-rule" />
+          <p className="main-body">
+            Dad would stage elaborate situations using a one-armed man to teach
+            us lessons. Smack of ham. What is she doing at a beauty pageant? Is
+            she running the lights or something? Chaw-chee, chaw-chee chaw-chee!
+            Coo coo ca cha! Ah coodle doodle do Caw ca caw, caw ca caw.{' '}
+          </p>
         </div>
+        <NavLink
+          exact
+          to="/donate-page"
+          className={
+            location.pathname !== '/' ? 'donate-btn-hidden' : 'donate-btn'
+          }
+        >
+          Donate
+        </NavLink>
       </section>
     );
   }
