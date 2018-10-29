c8700948d0aad19b35ba6323c266a1840b6d5760
Jesse McFadden <jessemcbrennan@gmail.com>
2018-10-29 10:20:42 -0600

    Add volunteer routes

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import DonatePage from '../../components/DonatePage/DonatePage';
 import AdminPage from '../AdminPage/AdminPage';
-import AboutPage from '../AboutPage/AboutPage';
+import VolunteersPage from '../VolunteersPage/VolunteersPage';
 import MissionPage from '../MissionPage/MissionPage';
 import PartnersPage from '../PartnersPage/PartnersPage';
 
@@ -12,7 +12,7 @@ export const Routes = () => {
       <Route exact path="/" />
       <Route exact path="/donate" component={DonatePage} />
       <Route exact path="/admin-page" component={AdminPage} />
-      <Route exact path="/about" component={AboutPage} />
+      <Route exact path="/volunteers" component={VolunteersPage} />
       <Route exact path="/mission" component={MissionPage} />
       <Route exact path="/partners" component={PartnersPage} />
     </Switch>
