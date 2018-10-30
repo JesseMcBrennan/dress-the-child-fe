6edd20a619b71361a7c71972d82f4037660767d4
Cody Taft <cody.taft@gmail.com>
2018-10-29 18:29:52 -0600

    Add volunteer and events path

--- src/components/MainContainer/MainContainer.js
+++ src/components/MainContainer/MainContainer.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-// import Routes from '../../components/Routes/Routes';
 import { withRouter, NavLink } from 'react-router-dom';
 import HomePage from '../HomePage/HomePage';
 import DonatePage from '../DonatePage/DonatePage';
-import VolunteersPage from '../VolunteersPage/VolunteersPage';
+import VolunteerPage from '../VolunteerPage/VolunteerPage';
 import MissionPage from '../MissionPage/MissionPage';
 import PartnersPage from '../PartnersPage/PartnersPage';
 import EventsPage from '../EventsPage/EventsPage';
+import GetInvolvedPage from '../GetInvolvedPage/GetInvolvedPage';
 import './MainContainer.css';
 
 export class MainContainer extends Component {
@@ -19,8 +19,9 @@ export class MainContainer extends Component {
         {location.pathname === `/donate` && <DonatePage />}
         {location.pathname === `/mission` && <MissionPage />}
         {location.pathname === `/partners` && <PartnersPage />}
-        {location.pathname === `/get-involved` && <VolunteersPage />}
+        {location.pathname === `/get-involved` && <GetInvolvedPage />}
         {location.pathname === `/events` && <EventsPage />}
+        {location.pathname === `/volunteer` && <VolunteerPage />}
       </section>
     );
   }
