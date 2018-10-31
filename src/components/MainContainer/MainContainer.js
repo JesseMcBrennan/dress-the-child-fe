8e326e2e24dce01a313685f35f8bdfd2f449028d
Cody Taft <cody.taft@gmail.com>
2018-10-30 18:27:01 -0600

    Style main container, especially margins in header

--- src/components/MainContainer/MainContainer.js
+++ src/components/MainContainer/MainContainer.js
@@ -4,9 +4,7 @@ import HomePage from '../HomePage/HomePage';
 import DonatePage from '../DonatePage/DonatePage';
 import VolunteerPage from '../VolunteerPage/VolunteerPage';
 import MissionPage from '../MissionPage/MissionPage';
-import PartnersPage from '../PartnersPage/PartnersPage';
 import EventsPage from '../EventsPage/EventsPage';
-import GetInvolvedPage from '../GetInvolvedPage/GetInvolvedPage';
 import './MainContainer.css';
 
 export class MainContainer extends Component {
@@ -18,8 +16,6 @@ export class MainContainer extends Component {
         {location.pathname === `/` && <HomePage />}
         {location.pathname === `/donate` && <DonatePage />}
         {location.pathname === `/mission` && <MissionPage />}
-        {location.pathname === `/partners` && <PartnersPage />}
-        {location.pathname === `/get-involved` && <GetInvolvedPage />}
         {location.pathname === `/events` && <EventsPage />}
         {location.pathname === `/volunteer` && <VolunteerPage />}
       </section>
