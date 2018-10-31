3837fe49fa43848f609ececaf13354fc723c4473
Cody Taft <cody.taft@gmail.com>
2018-10-31 15:37:35 -0600

    Remove routes from app container

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, withRouter, NavLink } from 'react-router-dom';
-import Routes from '../../components/Routes/Routes';
 import MainContainer from '../../components/MainContainer/MainContainer';
 import './App.css';
 import NavMain from '../../components/NavMain/NavMain';
