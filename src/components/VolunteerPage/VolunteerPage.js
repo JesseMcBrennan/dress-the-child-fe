5c1ca508f5043d35f0de0bc4f89219384a457054
Cody Taft <cody.taft@gmail.com>
2018-10-30 18:28:39 -0600

    Style volunteer page with events

--- src/components/VolunteerPage/VolunteerPage.js
+++ src/components/VolunteerPage/VolunteerPage.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import './VolunteerPage.css';
 
 const VolunteerPage = () => (
   <div className="display-card">
     <h1 className="main-title">Volunteer</h1>
     <hr className="horizontal-rule" />
-    <p className="main-body">
-      The mission of the Las Cruces Rio Grande Rotary Foundation’s Dress The
-      Child program is to provide new clothing and shoes for children living in
-      the poorest families in Dona Ana County. Many of these children have never
-      had new clothes and have never had the opportunity to shop for clothes
-      that might be appealing to them.
+    <p className="main-body-volunteer">
+      We spend our annual budget each year during November and December by
+      taking hundreds of children shopping.  We match each child with at least
+      one volunteer to take him or her through the store, and assist in making
+      good decisions.  Our shopping dates for 2018 are listed below:
     </p>
+    <ul>
+      <li className="shopping-event-list">
+        Sunday, November 4th at 6pm (sign-in at 5:30pm) Old Navy
+      </li>
+      <li className="shopping-event-list">
+        Sunday, November 18th at 6pm (sign-in at 5:30pm) Old Navy
+      </li>
+      <li className="shopping-event-list">
+        Sunday, December 2nd at 5:00pm (sign-in at 4:30pm) Kohl's
+      </li>
+      <li className="shopping-event-list">
+        Sunday, December 9th at 6pm (sign-in at 5:30) Old Navy
+      </li>
+    </ul>
   </div>
 );
 
