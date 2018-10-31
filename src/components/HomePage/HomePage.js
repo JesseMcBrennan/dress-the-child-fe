7c86e01b7c1fd3dca5c3b99b68a19119744b0507
Cody Taft <cody.taft@gmail.com>
2018-10-31 15:40:53 -0600

    Restyle donate button on home page

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -10,14 +10,6 @@ const HomePage = () => (
       <br />
       THE OPPORTUNITY TO SHOP FOR NEW WINTER CLOTHING
     </h1>
-    {/* <hr className="horizontal-rule" />
-    <p className="main-body">
-      Dress the Child, sponsored by the Rio Grande Rotary Club, is an event
-      allowing children and their families a chance to shop for new clothing.
-    </p>
-    <p className="main-body">
-      For some this will be their first ever retail experience.
-    </p> */}
     <NavLink exact to="/donate" className="donate-btn">
       Donate
     </NavLink>
