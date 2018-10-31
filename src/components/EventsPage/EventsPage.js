39009f54ee4b02ad76fac824b01a7e0dabde2599
Cody Taft <cody.taft@gmail.com>
2018-10-31 15:40:37 -0600

    Emphasive events on event page

--- src/components/EventsPage/EventsPage.js
+++ src/components/EventsPage/EventsPage.js
@@ -8,26 +8,26 @@ const EventsPage = () => (
     <h2 className="main-body-title">32nd Annual Dinner Details:</h2>
     <ul>
       <li className="event-details-item">
-        <i class="fa fa-calendar-alt" aria-hidden="true" />
+        <i className="fa fa-calendar-alt" aria-hidden="true" />
         Date: Sunday October 7th
       </li>
       <li className="event-details-item">
-        <i class="fa fa-clock" aria-hidden="true" />
+        <i className="fa fa-clock" aria-hidden="true" />
         Time: 6:00pm
       </li>
       <li className="event-details-item">
-        <i class="fa fa-map-marked-alt" aria-hidden="true" />
+        <i className="fa fa-map-marked-alt" aria-hidden="true" />
         Location: Las Cruces Convention Center
       </li>
       <li className="event-details-item">
-        <i class="fa fa-money-bill-alt" aria-hidden="true" />
+        <i className="fa fa-money-bill-alt" aria-hidden="true" />
         Cost: $60 per person
       </li>
     </ul>
     <p className="ticket-text">
       For tickets and additional information: <br />
-      Contact Boberg at 575-644-9469 or dougboberg@comcast.net. <br /> Or <br />
-      Contact Campbell at 505-220-5185 or vincent@primesalesmarketing.com.
+      Contact Boberg at 575-644-9469 or dougboberg@comcast.net <br /> Or <br />
+      Contact Campbell at 505-220-5185 or vincent@primesalesmarketing.com
     </p>
   </div>
 );
