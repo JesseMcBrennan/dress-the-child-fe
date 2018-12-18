95f001659e949c6e3c54913e843e1fc74f79741e
Amy Holt <amy.elizabeth.holt@gmail.com>
2018-12-18 09:02:22 -0700

    Change routes to main app and donate page

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import DonatePage from '../../components/DonatePage/DonatePage';
-import AdminPage from '../AdminPage/AdminPage';
-import VolunteersPage from '../VolunteersPage/VolunteersPage';
-import MissionPage from '../MissionPage/MissionPage';
-import PartnersPage from '../PartnersPage/PartnersPage';
+import HomePage from '../../components/HomePage/HomePage';
 
 export const Routes = () => {
   return (
     <Switch>
-      <Route exact path="/" />
-      <Route exact path="/donate" component={DonatePage} />
-      <Route exact path="/admin-page" component={AdminPage} />
-      <Route exact path="/volunteers" component={VolunteersPage} />
-      <Route exact path="/mission" component={MissionPage} />
-      <Route exact path="/partners" component={PartnersPage} />
+      <Route exact path="/" component={ HomePage }/>
+      <Route exact path="/donate" component={ DonatePage } />
     </Switch>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import { BrowserRouter } from 'react-router-dom';
-
+import Routes from './components/Routes/Routes';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <div>
+      <App />
+      <Routes />
+    </div>
   </BrowserRouter>,
   document.getElementById('root')
 );
